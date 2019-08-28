827ae8f061e52ed4685bc9dc63da8bf7cc7f5dfe
vale99nadin <valeria99_vz@hotmail.com>
2019-08-28 08:46:38 -0500

    :cat2: init.el

---
+++ init.el
@@ -0,0 +1,19 @@
+(package-initialize)
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 (quote
+		("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" default)))
+ '(package-selected-packages
+	 (quote
+		(all-the-icons-icon-for-buffer all-the-icons-install-fonts all-the-icons vue-mode web-mode nyan-mode undo-tree powerline org-bullets neotree multiple-cursors multi-term magit js2-mode highlight-indent-guides emojify company-tern ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
